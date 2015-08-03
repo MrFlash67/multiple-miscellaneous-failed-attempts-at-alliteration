fc908689f2c69d67ecd20a310db7adea90d9fc37
MrFlash67 <MrFlash67@users.noreply.github.com>
2015-08-03 20:59:04 +1200

    Better and more error-checkey!

--- spam.py
+++ spam.py
@@ -1,23 +1,23 @@
-import os, time, sys
-print 'message'
-msg = raw_input('> ')
-print 'times'
-times = int(raw_input('> '))
-print "You want to say " + msg + " " + str(times) + " times.\nPress enter to continue."
-raw_input()
-
-cmd = """
-osascript -e 'tell application "Skype" to activate
-tell application "System Events" to keystroke " """ + msg + """ "
-tell application "System Events" to keystroke return' 
-""".format(msg)
-
-#print 1
-print cmd
-i = 0
-#times = 17
 try:
-    while i < times:
+    import os, time, sys
+    print 'message'
+    msg = str(raw_input('> '))
+    print 'times'
+    times = int(raw_input('> '))
+    print "You want to say " + msg + " " + str(times) + " times.\nPress enter to continue."
+    raw_input()
+    
+    cmd = """
+    osascript -e 'tell application "Skype" to activate
+    tell application "System Events" to keystroke " """ + msg + """ "
+    tell application "System Events" to keystroke return' 
+    """.format(msg)
+    
+    #print 1
+    i = 0
+    #times = 17
+    
+    while i < times - 1:
         os.system(cmd)
         #time.sleep(0.1)
         print 'I said ' + msg + " and I have " + str(times - i) + " repitition(s) to go."
@@ -25,3 +25,5 @@ try:
         i = i + 1
 except KeyboardInterrupt:
     sys.exit()
+except ValueError:
+    print 'Ensure that all numbers are numbers.'
